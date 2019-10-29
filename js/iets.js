33fd56fba33cf72e4d0a8ca81a365e2e39882b95
Andreja Babic <andrejababic@outlook.com>
2019-10-29 20:09:00 +0100

    Bewerkingen

---
+++ js/iets.js
@@ -0,0 +1,12 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+/* aanroepen */
+var knop = document.querySelector();
+
+
+/* functie geven */
+function show() {
+
+}
